4f5aa9813a02514929503fbbb568ae6dc52907c7
FaztJs <fabianncdev@gmail.com>
2016-09-28 15:42:37 +0000

    authentication

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-var express = require('express');
 var path = require('path');
+
+var express = require('express');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var exphbs= require('express-handlebars');
 var mongoose = require('mongoose');
+var session = require('express-session');
+var passport = require('passport');
+var flash = require('connect-flash');
+var validator = require('express-validator');
+var MongoStore = require('connect-mongo')(session);
 
 var routes = require('./routes/index');
-
+var userRoutes = require('./routes/user');
 var app = express();
 
 mongoose.connect('localhost:27017/shopping');
+require('./config/passport');
 
 // view engine setup
 
@@ -26,10 +33,28 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
+app.use(session({
+  secret:'mysecretword',
+  resave:false,
+  saveUninitialized:false,
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  cookie: {maxAge: 180 * 60 * 1000 }
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req,res,next) {
+  res.locals.login = req.isAuthenticated();
+  res.locals.session = req.session;
+  next();
+});
+app.use('/user',userRoutes);
 app.use('/', routes);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
+
+var Cart = require('../models/cart');
 var Product = require('../models/product');
-
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,12 +13,24 @@ router.get('/', function(req, res, next) {
     for(var i = 0; i < docs.length; i += chunckSize){
       productChunk.push(docs.slice(i, i + chunckSize));
     }
-    console.log(productChunk);
     res.render('shop/index', { title: 'Shopping Cart',products:productChunk});
   });
 
 });
 
+router.get('/add-to-cart/:id',function(req,res,next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {});
 
+  Product.findById(productId,function(err, product) {
+    if(err){
+      return res.redirect('/');
+    }
+    cart.add(product,product.id);
+    req.session.cart = cart;
+    console.log(req.session.cart);
+    res.redirect('/');
+  });
+});
 
 module.exports = router;
